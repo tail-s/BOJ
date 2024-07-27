851ba6bbc3dbf3914a02205eb9f9798988a03048
oliver's life <98503573+tail-s@users.noreply.github.com>
2024-07-28 05:12:05 +0900

    [Silver II] Title: 배열 돌리기, Time: 1088 ms, Memory: 176472 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
@@ -1,71 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int n, a;
-    static int[][] map, ans, d;
-    static int[] NA = new int[2];
-    static String[] arr = {"+0", "+1", "+2", "+3", "-0", "-1", "-2", "-3", "+0", "+1", "+2", "+3"};
-    static int[] reverse(int[] arr) {
-        int[] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) result[i] = arr[arr.length - 1 - i];
-        return result;
-    }
 
-    static int check(int a) {
-        int b = Character.getNumericValue(arr[a].charAt(1));
-        if (arr[a].charAt(0) == '-') d[b] = reverse(d[b]);
-        return b;
-    }
-
-    static void solve() {
-        int rot = (a / 45) % 8;
-        if (rot < 0) rot += 8;
-
-        int idx = check(rot);
-        for (int i = 0; i < n; i++) ans[i][i] = d[idx][i];
-
-        idx = check(rot + 1);
-        for (int i = 0; i < n; i++) ans[n / 2][i] = d[idx][i];
-
-        idx = check(rot + 2);
-        for (int i = 0; i < n; i++) ans[n - i - 1][i] = d[idx][i];
-
-        idx = check(rot + 3);
-        for (int i = 0; i < n; i++) ans[n - i - 1][n / 2] = d[idx][i];
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) sb.append(ans[i][j] == 0 ? map[i][j] : ans[i][j]).append(" ");
-            sb.setLength(sb.length() - 1);
-            sb.append("\n");
-        }
-    }
-
+    static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
-            NA = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            n = NA[0];
-            a = NA[1];
-            map = new int[n][n];
-            ans = new int[n][n];
-            d = new int[4][n];
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); // 배열 크기
+            int d = Integer.parseInt(st.nextToken()); // 각도
+            map = new int[N][N];
+            for(int i=0; i<N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-            for (int i = 0; i < n; i++) map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            for (int i = 0; i < n; i++) d[0][i] = map[i][i];        // 우하향
-            for (int i = 0; i < n; i++) d[1][i] = map[n / 2][i];    // 가운데행
-            for (int i = 0; i < n; i++) d[2][i] = map[n - i - 1][i];// 우상향
-            for (int i = 0; i < n; i++) d[3][i] = map[n - i - 1][n / 2];// 가운데열
+            // 배열 돌리기
+            d /= 45;
+            if(d < 0)  d += 8;
+            int len = N-(N-1)/2-1;
+            int len2 = N;
+            for(int i=0; i<len; i++) {
+                rotate(i, (len2-1)/2, len2-1, d);
+                len2 -= 2;
+            }
 
-            solve();
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    sb.append(map[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
         }
-
         System.out.println(sb);
     }
+
+    public static void rotate(int x, int half, int N, int d) {
+        for(int k=0; k<d; k++){
+            int temp = map[x][x]; // 시작점
+            // 상
+            for(int i=0; i<2; i++) map[x+(half*i)][x] = map[x+half*(1+i)][x];
+            // 좌
+            for(int i=0; i<2; i++) map[x+N][x+(half*i)] = map[x+N][x+half*(1+i)];
+            // 하
+            for(int i=1; i>=0; i--) map[x+half*(1+i)][x+N] = map[x+(half*i)][x+N];
+            // 우
+            for(int i=1; i>=0; i--) map[x][x+half*(1+i)] = map[x][x+half*i];
+            map[x][x+half] = temp;
+        }
+    }
+
 }
