690430ce52c789aa6c58563c2f7f03d14577116f
oliver's life <98503573+tail-s@users.noreply.github.com>
2024-09-14 19:26:18 +0900

    [Silver V] Title: 수들의 합, Time: 92 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,20 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static long S, ans;
+    static long _sum(long N) { return (1 + N) * N / 2; }
+
+    static void binarySearch(long s, long e) {
+
+        if (s > e) return;
+        long mid = (s + e) / 2;
+        long sum = _sum(mid);
+
+        if (sum > S) binarySearch(s, mid - 1);
+        else if (sum < S) {
+            ans = Math.max(ans, mid);
+            binarySearch(mid + 1, e);
+        }
+        else ans = mid;
+
+
+//        while (s <= e) {
+//            long mid = (s + e) / 2;
+//            long sum = _sum(mid);
+//
+//            if (sum <= S) {
+//                ans = mid;
+//                s = mid + 1;
+//            } else e = mid - 1;
+//        }
+
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long S = sc.nextLong();
+        S = sc.nextLong();
         sc.close();
+        ans = 0L;
 
-        Long sum = 0L;
-        int ans = 0;
+//        for (int i = 1; sum <= S; i++) {
+//            sum += i;
+//            ans++;
+//        }
+//        ans--;
+//        System.out.println(ans);
 
-        for (int i = 1; sum <= S; i++) {
-            sum += i;
-            ans++;
-        }
-        ans--;
+        binarySearch(1, S);
         System.out.println(ans);
+
+
 
     }
 }
