462688ecc84cd6fa16eb9b8e8157a50aa920a07f
oliver's life <98503573+tail-s@users.noreply.github.com>
2024-09-14 21:14:24 +0900

    [Silver V] Title: 숫자 카드, Time: 1676 ms, Memory: 311684 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,19 +1,47 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    static int N, M;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-
         StringBuilder sb = new StringBuilder();
-        HashSet<Integer> target = new HashSet<Integer>();
-        while (N-- > 0) target.add(sc.nextInt());
-        M = sc.nextInt();
-        while (M-- > 0) sb.append(target.contains(sc.nextInt()) ? "1 " : "0 ");
-        sb.setLength(sb.length()-1);
-        sc.close();
+
+//        int N = sc.nextInt();
+//        HashSet<Integer> target = new HashSet<Integer>();
+//        while (N-- > 0) target.add(sc.nextInt());
+//        int M = sc.nextInt();
+//        while (M-- > 0) sb.append(target.contains(sc.nextInt()) ? "1 " : "0 ");
+//        sb.setLength(sb.length()-1);
+//        sc.close();
+//        System.out.println(sb.toString());
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        int M = sc.nextInt();
+        while (M-- > 0) {
+            int target = sc.nextInt();
+            int s = 0;
+            int e = N - 1;
+            while (s <= e) {
+                int mid = (s + e) / 2;
+                int val = arr[mid];
+                if (val == target) {
+                    sb.append("1 ");
+                    break;
+                }
+
+                if (val > target) e = mid - 1;
+                else s = mid + 1;
+
+                if (s > e) {
+                    sb.append("0 ");
+                    break;
+                }
+            }
+        }
         System.out.println(sb.toString());
     }
 }
