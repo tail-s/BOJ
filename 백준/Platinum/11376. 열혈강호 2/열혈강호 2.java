0764d4fe11d300ed2dd43ca72e38e631d8718136
oliver's life <98503573+tail-s@users.noreply.github.com>
2024-09-24 19:53:08 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 5192 ms, Memory: 320188 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] task;  // task를 맡고있는 사람의 정보
+    static boolean[] clear; // 특정 task를 처리했는지에 대한 정보
+    static List<Integer>[] lists;
+
+    static boolean dfs(int cur) {
+        for (int i : lists[cur]) {
+            if (clear[i]) continue;
+            clear[i] = true;
+
+            if (task[i] == 0 || dfs(task[i])) {
+                task[i] = cur;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        lists = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) lists[i] = new ArrayList<>();
+
+        task = new int[M + 1];
+        clear = new boolean[M + 1];
+        for (int i = 1; i <= N; i++) {
+            int taskNum = sc.nextInt();
+            while (taskNum-- > 0) lists[i].add(sc.nextInt());
+        }
+        sc.close();
+
+        int ans = 0;
+        for (int i = 1; i <= N; i++) for (int j = 0; j < 2; j++) {
+            if (dfs(i)) ans++;
+            Arrays.fill(clear, false);
+        }
+
+        System.out.println(ans);
+
+    }
+    
+}
